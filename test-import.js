82e48c2b1882a4366553e496b1864f471cb40695
raulalbertdev <raulalbertdev@gmail.com>
2025-04-19 23:47:26 -0500

    Añadir un test-import de un modulo que da error en produccionPrueba 2

--- test-import.js
+++ test-import.js
@@ -1,3 +1,4 @@
-import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
+// import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
+import { BullMQAdapter } from '@bull-board/api/dist/cjs/queueAdapters/bullMQ.js';
 
 console.log("✅ Importación exitosa");
